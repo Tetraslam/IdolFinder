de6c79b7669e3e8073d05820872e333f636442e2
Tetraslam <bhowmickshresht@gmail.com>
2023-05-09 09:24:27 +0530

    added jeongyeon

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import face_recognition
 # Loop through loads and encodings to enable UI to detect smoothly
-known_image = face_recognition.load_image_file("./training/nayeon.jpg")
-unknown_image = face_recognition.load_image_file("./validation/valLisa.jpg")
+nayeon = face_recognition.load_image_file("./training/nayeon.jpg")
+jeongyeon = face_recognition.load_image_file("./training/jeongyeon.jpg")
 
-nayeon_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_image = face_recognition.load_image_file("./validation/valNayeon.jpg")
+
+nayeon_encoding = face_recognition.face_encodings(nayeon)[0]
+jeongyeon_encoding = face_recognition.face_encodings(jeongyeon)[0]
+
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 results = face_recognition.compare_faces([nayeon_encoding], unknown_encoding)
